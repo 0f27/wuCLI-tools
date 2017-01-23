1d9f6f84f8792e509299a22ae6f576bd53dbeb2d
0f27 <x0f27@yahoo.com>
2017-01-23 17:55:08 +0300

    new tool - grab russian towns from wiki in csv format

---
+++ rustowns_ru.sh
@@ -0,0 +1,1 @@
+curl -sL https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%B3%D0%BE%D1%80%D0%BE%D0%B4%D0%BE%D0%B2_%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8 | sed -n '/standard sortable/,/\/table/p' | tr -d '\n' | sed -r 's/<\/?tr[^<>]*>/\n/g; s/<\/?t[dh][^<>]*>/,/g; s/<[^<>]+>|&#160;|\[[^\[\]]*\]//g; s/(↘|↗)/\1,/g' | awk -F, 'BEGIN {OFS=","} $6!~/^$/ {print $2,$6,$8,$10,$12,$14}'
