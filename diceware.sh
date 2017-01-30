f0dcc3c11af1813105225aa64c4a756132127fa8
0f27 <x0f27@yahoo.com>
2017-01-30 18:55:56 +0300

    optimisation

--- diceware.sh
+++ diceware.sh
@@ -1,3 +1,1 @@
-
-
-for i in {1..7}; do curl -s http://world.std.com/~reinhold/diceware.wordlist.asc | awk '/'"$[ ( $RANDOM % 6 ) +1 ]$[ ( $RANDOM % 6 ) +1 ]$[ ( $RANDOM % 6 ) +1 ]$[ ( $RANDOM % 6 ) +1 ]$[ ( $RANDOM % 6 ) +1 ]"'/ {print $_}'; done
+curl -s http://world.std.com/~reinhold/diceware.wordlist.asc > dw.txt ; for i in {1..7}; do < dw.txt awk '/'"$[ ( $RANDOM % 6 ) +1 ]$[ ( $RANDOM % 6 ) +1 ]$[ ( $RANDOM % 6 ) +1 ]$[ ( $RANDOM % 6 ) +1 ]$[ ( $RANDOM % 6 ) +1 ]"'/ {print $2}'; done; rm dw.txt
